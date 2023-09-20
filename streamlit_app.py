c5423596115bdbce025cea1c137ff2dd888a26ce
Ankit <129873373+fnuankit164@users.noreply.github.com>
2023-09-19 23:07:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,6 @@ st.text('🥑🍞 Avacado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+st.multiselect("Pick some fruits:" list(my_fruit_list.index))
 st.dataframe(my_fruit_list)
